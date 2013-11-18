74b4974a9765f3f6a555c87f2346d401f676833e
Wonjun Kim <wonjun.kim@navercorp.com>
2013-11-18 10:29:46 +0900

    [issue-39] javadoc documentation for IndicatorLayoutFactory

--- library/src/main/java/com/handmark/pulltorefresh/library/IndicatorLayoutFactory.java
+++ library/src/main/java/com/handmark/pulltorefresh/library/IndicatorLayoutFactory.java
@@ -12,40 +12,32 @@ import com.handmark.pulltorefresh.library.PullToRefreshBase.Mode;
 import com.handmark.pulltorefresh.library.internal.IndicatorLayout;
 import com.handmark.pulltorefresh.library.internal.DefaultIndicatorLayout;
 import com.handmark.pulltorefresh.library.internal.Utils;
-
+/**
+ * Factory which creates indicator layouts 
+ * <br />Indicator layouts must be listed in pulltorefresh.xml as "PullToRefresh/IndicatorLayouts/layout" nodes
+ * @author Wonjun Kim
+ */
 public class IndicatorLayoutFactory {
 
 	private static final String LOG_TAG = IndicatorLayoutFactory.class.getName();
 	/**
-	 * 
-	 * @param layoutCode
-	 * @param context
-	 * @param mode
-	 * @return
-	 */
-	public static IndicatorLayout createIndicatorFactory(String layoutCode, Context context, PullToRefreshBase.Mode mode) {
-		String clazzName = PullToRefreshXmlConfiguration.getInstance().getIndicatorLayoutClazzName(layoutCode);
-		Class<? extends IndicatorLayout> clazz = createIndicatorLayoutClazz(clazzName);
-		return createIndicatorLayout(layoutCode, context, mode);
-	}
-	/**
-	 * 
-	 * @param layoutCode
-	 * @return
+	 * Create the class token matched by <b>{@code layoutCode}</b>
+	 * @param layoutCode Indicator layout code, which must be defined in pulltorefresh.xml 
+	 * @return Class token which is matched by {@code layoutCode}, or the class token of {@code DefaultIndicatorLayout} instance if not
 	 */
 	public static Class<? extends IndicatorLayout> createIndicatorLayoutClazzByLayoutCode(String layoutCode) {
 		String clazzName = PullToRefreshXmlConfiguration.getInstance().getIndicatorLayoutClazzName(layoutCode);
 		return createIndicatorLayoutClazz(clazzName);
 	}
 	/**
-	 * 
-	 * @param clazzName
-	 * @return
+	 * Create the class token matched by <b>class name</b>
+	 * @param clazzName Class name such as "com.handmark.pulltorefresh.library.internal.DefaultIndicatorLayout"
+	 * @return Class token if the class matched by class name exists, or the class token of {@code DefaultIndicatorLayout} instance if not  
 	 */
 	@SuppressWarnings("unchecked")
 	public static Class<? extends IndicatorLayout> createIndicatorLayoutClazz(String clazzName) {
 		Class<? extends IndicatorLayout> clazz = null;
-		if ( clazzName == null) {
+		if (clazzName == null) {
 			clazz = DefaultIndicatorLayoutFactory.createIndicatorLayoutClazz(clazzName);
 			return clazz;
 		}
@@ -62,24 +54,22 @@ public class IndicatorLayoutFactory {
 		return clazz;
 	}
 	/**
-	 * 
-	 * @param layoutCode
-	 * @param context
-	 * @param mode
-	 * @return
+	 * Create a {@code IndicatorLayout} instance matched by <b>{@code layoutCode}</b> 
+	 * @param layoutCode Indicator layout code, which must be defined in pulltorefresh.xml
+	 * @param context 
+	 * @param mode 
+	 * @return {@code IndicatorLayout} instance if the class matched by {@code layoutCode} exists, or {@code DefaultIndicatorLayout} instance if not  
 	 */
 	public static IndicatorLayout createIndicatorLayout(String layoutCode, Context context, PullToRefreshBase.Mode mode) {
 		Class<? extends IndicatorLayout> clazz = createIndicatorLayoutClazz(layoutCode);
 		return createIndicatorLayout(clazz, context, mode);
 	}
-	
 	/**
-	 * 
-	 * @param context
-	 * @param mode
-	 * @param attrs
-	 * @comment Need to be refactored!
-	 * @return
+	 * Create a {@code IndicatorLayout} instance matched by <b>{@code clazz} token</b> 
+	 * @param layoutCode Indicator layout code, which must be defined in pulltorefresh.xml
+	 * @param context 
+	 * @param mode 
+	 * @return {@code IndicatorLayout} instance if the class matched by {@code layoutCode} exists, or {@code DefaultIndicatorLayout} instance if not  
 	 */
 	public static IndicatorLayout createIndicatorLayout(
 			Class<? extends IndicatorLayout> clazz, Context context, Mode mode) {
@@ -114,24 +104,26 @@ public class IndicatorLayoutFactory {
 		return layout;
 	}	
 	/**
-	 * 
-	 * @author NBP
+	 * Create a default indicator layout instance. This is used when {@code IndicatorLayoutFactory} fails to create a instance
+	 * @author Wonjun Kim
 	 *
 	 */
 	private static class DefaultIndicatorLayoutFactory {
+		/**
+		 * @param clazzName This class name is being ignored
+		 * @return Class token of {@code DefaultIndicatorLayout}
+		 */
 		public static Class<? extends IndicatorLayout> createIndicatorLayoutClazz(String clazzName) {
 			return DefaultIndicatorLayout.class;
 		}
 		/**
-		 * 
-		 * @param clazz
+		 * @param clazz Class token is being ignored.
 		 * @param context
 		 * @param mode
-		 * @return
+		 * @return {@code DefaultIndicatorLayout} instance
 		 */
 		public static IndicatorLayout createIndicatorLayout(Class<? extends IndicatorLayout> clazz, Context context, PullToRefreshBase.Mode mode) {
 			return new DefaultIndicatorLayout(context, mode);
 		}
-		
 	}
 }
