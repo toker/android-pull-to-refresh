4a80ed32a229e1f6e1dcee816b9c8baeb2dbfc01
Wonjun Kim <wonjun.kim@navercorp.com>
2013-11-18 15:37:53 +0900

    [issue-42] javadoc documentation for IndicatorLayout

--- library/src/main/java/com/handmark/pulltorefresh/library/internal/IndicatorLayout.java
+++ library/src/main/java/com/handmark/pulltorefresh/library/internal/IndicatorLayout.java
@@ -3,9 +3,12 @@ package com.handmark.pulltorefresh.library.internal;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.widget.FrameLayout;
-
-public abstract class IndicatorLayout extends FrameLayout implements
-		IIndicatorLayout {
+/**
+ * @see IIndicatorLayout
+ * @author Wonjun Kim
+ */
+public abstract class IndicatorLayout extends FrameLayout implements IIndicatorLayout {
+	
 	public IndicatorLayout(Context context) {
 		super(context);
 	}
@@ -14,12 +17,20 @@ public abstract class IndicatorLayout extends FrameLayout implements
 		super(context, attrs);
 	}
 
-	public IndicatorLayout(Context context, AttributeSet attrs,
-			int defStyle) {
+	public IndicatorLayout(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
 	}
-	
+	/**
+	 * Create a specific {@code LayoutParams}.<br /> 
+	 * Pull To Refresh will add this layout with applying this {@code LayoutParams} to the layout     
+	 * @return {@code LayoutParams} which is applied if this indicator layout is a header of Pull To Refresh
+	 */
 	public abstract LayoutParams createApplicableHeaderLayoutParams();
+	/**
+	 * Create a specific {@code LayoutParams}.<br /> 
+	 * Pull To Refresh will add this layout with applying this {@code LayoutParams} to the layout     
+	 * @return {@code LayoutParams} which is applied if this indicator layout is a footer of Pull To Refresh 
+	 */
 	public abstract LayoutParams createApplicableFooterLayoutParams();
 
 }
